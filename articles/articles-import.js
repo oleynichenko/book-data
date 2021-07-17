3dcac09ed3fef17542d8e8f9d9451324a6e6ee9f
Oleynichenko <oleynichenkos@gmail.com>
2021-07-17 19:55:22 +0300

    add foreword-tes 1, 2, 18 data

--- articles/articles-import.js
+++ articles/articles-import.js
@@ -1,21 +1,19 @@
 const fs = require(`fs`);
 
 const data = {
-  a1: require(`articles/pticha/ru-zh/pticha-ru-zh-41`),
-  a2: require(`articles/pticha/ru-zh/pticha-ru-zh-42`),
-  a3: require(`articles/pticha/ru-zh/pticha-ru-zh-43`),
-  a4: require(`articles/pticha/ru-zh/pticha-ru-zh-44`),
-  a5: require(`articles/pticha/ru-zh/pticha-ru-zh-45`),
-  a6: require(`articles/pticha/ru-zh/pticha-ru-zh-46`),
-  a7: require(`articles/pticha/ru-zh/pticha-ru-zh-47`),
-  a8: require(`articles/pticha/ru-zh/pticha-ru-zh-48`),
-  a9: require(`articles/pticha/ru-zh/pticha-ru-zh-49`),
-  a10: require(`articles/pticha/ru-zh/pticha-ru-zh-50`),
+  a1: require(`./foreword-tes/ru-zh/foreword-tes-ru-zh-1`),
+  a2: require(`./foreword-tes/ru-zh/foreword-tes-ru-zh-2`),
+  a3: require(`./foreword-tes/ru-bb/foreword-tes-ru-bb-1`),
+  a4: require(`./foreword-tes/ru-bb/foreword-tes-ru-bb-2`),
+  a5: require(`./foreword-tes/he-bs/foreword-tes-he-bs-1`),
+  a6: require(`./foreword-tes/he-bs/foreword-tes-he-bs-2`),
+  a7: require(`./foreword-tes/en-bb/foreword-tes-en-bb-1`),
+  a8: require(`./foreword-tes/en-bb/foreword-tes-en-bb-2`),
 };
 
 let articles = '';
 
-for(let i = 1; i <= 10; i++) {
+for(let i = 1; i <= 8; i++) {
   articles = articles + JSON.stringify(data[`a${i}`]).slice(1,-1) + '\n';
 }
 
